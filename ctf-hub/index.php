88aac141bef7e6040eb85f763733752522059c8c
pevin <pevinbalaji@gmail.com>
2025-08-05 13:29:06 +0000

    add initial structure

---
+++ ctf-hub/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CTF-HUB | Home</title>
+</head>
+
+<body>
+    <div>
+        <h1>CTF-HUB</h1>
+    </div>
+</body>
+
+</html>
